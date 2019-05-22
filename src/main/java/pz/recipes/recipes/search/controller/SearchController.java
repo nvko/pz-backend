bb6a54158b3538a4495be86d6e368e1b668ecbfc
Kredensinh0 <ksawery.hasnik@gmail.com>
2019-05-22 21:45:43 +0200

    fixed some issued

--- src/main/java/pz/recipes/recipes/search/controller/SearchController.java
+++ src/main/java/pz/recipes/recipes/search/controller/SearchController.java
@@ -13,11 +13,11 @@ public class SearchController {
 
     @Autowired SearchService searchService;
 
-    @GetMapping("/recipes")
-    public ResponseEntity<?> findRecipesByQuery(@RequestParam(value = "query", defaultValue = "") String query,
-                                                @RequestParam(value = "page", defaultValue = "1") int page,
-                                                @RequestParam(value = "limit", defaultValue = "10") int limit) {
-        return new ResponseEntity<>(new SearchResponse<>(searchService.findAllRecipesByQuery(query, page, limit)), HttpStatus.OK);
-    }
+//    @GetMapping("/recipes")
+//    public ResponseEntity<?> findRecipesByQuery(@RequestParam(value = "query", defaultValue = "") String query,
+//                                                @RequestParam(value = "page", defaultValue = "1") int page,
+//                                                @RequestParam(value = "limit", defaultValue = "10") int limit) {
+//        return new ResponseEntity<>(new SearchResponse<>(searchService.findAllRecipesByQuery(query, page, limit)), HttpStatus.OK);
+//    }
 
 }
