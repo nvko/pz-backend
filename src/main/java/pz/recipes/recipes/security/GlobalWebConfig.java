eb93a673dce72ebfd1ee0a33184ac8148ca37ace
Kredensinh0 <ksawery.hasnik@gmail.com>
2019-05-22 23:42:35 +0200

    fixed cors

--- src/main/java/pz/recipes/recipes/security/GlobalWebConfig.java
+++ src/main/java/pz/recipes/recipes/security/GlobalWebConfig.java
@@ -11,6 +11,6 @@ public class GlobalWebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "DELETE", "POST", "GET");
     }
 }
